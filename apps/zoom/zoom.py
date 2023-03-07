c118dd4c452dd936492192c231c603ea6804b2cd
Lianna Eeftinck <liannaee@gmail.com>
2023-03-07 23:55:34 +0000

    Add zoom.py from Andreas

---
+++ apps/zoom/zoom.py
@@ -0,0 +1,22 @@
+from talon import Context, actions, Module
+
+ctx = Context()
+mod = Module()
+
+mod.apps.zoom = """
+os: windows
+and app.name: Zoom Meetings
+os: windows
+and app.exe: Zoom.exe
+"""
+
+ctx.matches = r"""
+app: zoom
+"""
+
+ctx.tags = ["user.voip"]
+
+@ctx.action_class("user")
+class UserActions:
+    def mute_microphone():
+        actions.key("alt-a")
